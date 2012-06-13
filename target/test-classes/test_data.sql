29ea176a4c6ffbf8b0db9789a159e7d639411d2c
pszanto <pszanto@sw-hubu-1528>
2012-06-13 11:58:34 +0200

    deleting target

--- target/test-classes/test_data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO promotion (name) VALUES ('test promotion 1');
-INSERT INTO promotion (name) VALUES ('test promotion 2');
-
-INSERT INTO site (brand_code, country_code, language_code, login_time_limit, default_promotion_id) VALUES ('TST', 'UK', 'EN', 10, 1);
-INSERT INTO site (brand_code, country_code, language_code, login_time_limit, default_promotion_id) VALUES ('TS2', 'UK', 'EN', 10, 2);
-
